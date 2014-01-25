0cbcf035aa99ca6d51e7e328c258bf0520439f55
Nadya <nadyacpena@gmail.com>
2014-01-25 14:40:45 -0500

    Added Character class
    This class creates the physical characters with images.

---
+++ characters.java
@@ -0,0 +1,20 @@
+/**
+* Class make characters - this class creates the characters
+*	
+*/
+import javax.imageio.*;
+import java.awt.image.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class characters{
+
+	private BufferedImage jodineimg, gesperimg, williyskyimg, bookerimg;
+	
+	public void displayjodine(){
+		
+		System.out.println("this shit works");
+	}
+}
